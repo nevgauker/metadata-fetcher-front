39d10c00ebb7405a8074666f0de0ca84693c3962
rotem <nevgauker@gmail.com>
2024-08-20 00:37:30 +0100

    loader and some ui fixes

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { fetchMetadata, Metadata } from "@/actions/fetch-metadata";
 
 import ResultCard from "@/components/result-card";
+import ProgressLoader from "@/components/progress-indicator";
 
 export default function Home() {
 
@@ -17,6 +18,7 @@ export default function Home() {
   ]);
   const [error, setError] = useState(false)
   const [results, setRsults] = useState<Metadata[]>([])
+  const [loading, setLoading] = useState(false)
 
   const addRow = () => {
     setRows((prevRows) => [
@@ -54,19 +56,19 @@ export default function Home() {
 
   const handleFetchMetadata = async () => {
     setError(false)
-
+    setLoading(true)
     const urls = rows.map((row) => row.selectValue + row.inputValue);
-
     // Validate input length and URL format
     for (const url of urls) {
       const inputValue = url.replace(/https?:\/\//, ""); // Remove the protocol for length check
       if (inputValue.length <= 2) {
         setError(true)
+        setLoading(false)
         return;
       }
-
       if (!validateUrl(url)) {
         setError(true)
+        setLoading(false)
         return;
       }
     }
@@ -75,30 +77,41 @@ export default function Home() {
       const metadata = await fetchMetadata(urls);
       console.log("Fetched metadata:", metadata);
       setRsults(metadata)
+      setLoading(false)
+
       // Handle the fetched metadata (e.g., display it to the user)
     } catch (error) {
+      setLoading(false)
       alert("Failed to fetch metadata. Please try again later.");
     }
   };
 
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl">Fetch Metadata From Urls</h1>
-
-      <h2 className="text-xl underline py-4">Urls</h2>
-      <div className="flex flex-col space-y-4 border-b-2">
-        {renderRows()}
-        {error && <p className="text-red-700 text-sm">Pleas write valid urls</p>}
-        <Button className="text-lg" variant="outline" onClick={addRow}>+</Button>
-      </div>
-      <div className="flex justify-center items-center">
-        <Button className="py-4" variant="outline" onClick={handleFetchMetadata}>Fetch Metadata</Button>
-      </div>
-      {results.length > 0 && <h2 className="text-xl underline py-4">Results</h2>}
-      <div className="grid grid-cols-1 md:grid-cols-3 space-x-1 space-y-1">
-        {results.map((result) => <ResultCard url={result.url} description={result.description} image={result.image} error={result.error} />)}
-      </div>
-
+      {loading ? <ProgressLoader /> : <>
+        <h2 className="text-xl underline py-4">Urls</h2>
+        <div className="flex flex-col space-y-4 border-b-2">
+          {renderRows()}
+          {error && <p className="text-red-700 text-sm">Pleas write valid urls</p>}
+          <Button className="text-lg" variant="outline" onClick={addRow}>+</Button>
+        </div>
+        <div className="flex justify-center items-center py-4">
+          <Button className="py-4" variant="outline" onClick={handleFetchMetadata}>Fetch Metadata</Button>
+        </div>
+        {results.length > 0 && <h2 className="text-xl underline py-4">Results</h2>}
+        <div className="grid grid-cols-1 md:grid-cols-3 space-x-1 space-y-1">
+          {results.map((result) => <ResultCard url={result.url} description={result.description} image={result.image} error={result.error} />)}
+        </div>
+      </>
+      }
     </main>
   );
+}
+
+
+function Body() {
+
+
 }
